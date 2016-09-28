fe1ee7773375822b93de11e8e8e32aea86cfb9f7
Simon Richter <simon.richter@hotmail.com>
2016-09-28 08:15:23 +0200

    Timer using PWM Mode 1

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ main (int argc, char* argv[])
   // enable clock on GPIO A
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
   // set as output
-  GPIOA->MODER |= GPIO_MODER_MODER5_0;
+  GPIOA->MODER |= GPIO_MODER_MODER5_1;
   // set to low speed (2MHz)
   GPIOA->OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR5);
   GPIOA->AFR[0] |= (GPIO_AF1_TIM2 << (5*4));
@@ -32,19 +32,38 @@ main (int argc, char* argv[])
   /**
    * initialize the timer
    */
-  // enable timer 2
+  // Wie schon zuvor
+  // enable clock to timer 2
   RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-  // set prescaler value
-  // use -1 because the event gets fired one clock later
-  TIM2->PSC = 10000 -1; // clock = 100.000.000 Hz -> timer_clk = 10.000 Hz
-  // count to 10000
-  TIM2->ARR = 10000; // PWM frequency 1 Hz
-  TIM2->CCR1 = TIM2->ARR / 2; // duty cycle 0.5 second
 
-  TIM2->CCMR1 |= TIM_CCMR1_OC1M_0 || TIM_CCMR1_OC1M_1;
-  TIM2->CCMR1 |= TIM_CCMR1_OC1PE; // enable preload register
-  TIM2->CR1 |= TIM_CR1_ARPE; // enable auto-reload preload
-  TIM2->EGR |= TIM_EGR_UG; // create update event to load all values to shadow registers
+  // set prescaler value
+  TIM2->PSC = 10000 -1; // clock = 100.000.000 Hz -> CK_PSK = 10.000 Hz
+
+  // count to 5000
+  TIM2->ARR = 5000 -1; // 10.000 Hz / 5000 = 2 Hz
+
+  TIM2->CCR1 = 2500 -1;
+
+
+  // Ab hier dann capture compare setup
+
+  // set capture compare mode to "pwm mode 1", i.e. output active if CNT<CCR
+  TIM2->CCMR1 = TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
+
+  // enable the capture compare output
+  TIM2->CCER = TIM_CCER_CC1E;
+
+  // enable counter
+  TIM2->CR1 |= TIM_CR1_CEN;
+
+
+  // Ab hier dann capture compare setup
+
+  // set capture compare mode to "pwm mode 1", i.e. output active if CNT<CCR
+  TIM2->CCMR1 = TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
+
+  // enable the capture compare output
+  TIM2->CCER = TIM_CCER_CC1E;
 
   // enable counter
   TIM2->CR1 |= TIM_CR1_CEN;
