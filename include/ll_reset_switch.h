bd0f3d75a69dcb82da0392f9ac2596645bd723b6
Simon Richter <simon.richter@hotmail.com>
2016-10-21 15:31:53 +0200

    removed unused defines

--- include/ll_reset_switch.h
+++ include/ll_reset_switch.h
@@ -3,9 +3,6 @@
 
 #include "stm32f411xe.h"
 
-#define LL_RESET_SWITCH_PORT	(2)
-#define LL_RESET_SWITCH_PIN		()
-
 void ll_reset_switch_init(void);
 void ll_reset_switch_enable(void);
 void ll_reset_switch_disable(void);
