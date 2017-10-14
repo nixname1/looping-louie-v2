e51bc3060d207fcf8e5b1cef3b2239d5007befb3
Simon Richter <simon.richter@hotmail.com>
2017-10-14 17:17:35 +0200

    added ring animation to start animation

--- src/anim/game_start.c
+++ src/anim/game_start.c
@@ -1,17 +1,62 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "ll_led.h"
 #include "ll_anim.h"
 #include "anim/game_start.h"
+#include "../../../simulation_renderer.h"
+
+struct payload
+{
+	uint32_t led_counter;
+};
 
 static uint32_t start_animation(struct color *framebuffer, void *payload)
 {
+	struct payload *p = payload;
+
+	p->led_counter = 0;
+
     return 1;
 }
 
 static uint32_t run_animation(struct color *framebuffer, void *payload)
 {
-    return 1;
+	static uint32_t cnt = 0;
+	static uint32_t ring_cnt = 0;
+	struct payload *p = payload;
+	struct color *col;
+	uint32_t player_number = (p->led_counter) / (LL_LED_NUM_STRIPE_PER_PLAYER);
+
+	if(p->led_counter >= LL_LED_NUM_STRIPE)
+	{
+		return 1;
+	}
+
+	col = ll_player_get_color(player_number);
+	ll_led_set_pixel(framebuffer, col, LL_LED_NUM_ALL_BARS_CIRCLES + p->led_counter);
+	simulation_renderer_render_frame(framebuffer);
+
+	if(p->led_counter > LL_LED_NUM_BAR_CIRCLE_PER_PLAYER * player_number)
+	{
+		cnt++;
+	}
+	else
+	{
+		cnt = 0;
+		ring_cnt = 0;
+	}
+
+	if(cnt >= 6)
+	{
+		ll_led_set_pixel_for_player(framebuffer, col, LL_LED_NUM_PER_BAR + ring_cnt, player_number);
+		simulation_renderer_render_frame(framebuffer);
+		ll_led_set_pixel_for_player(framebuffer, col, LL_LED_NUM_PER_BAR + LL_LED_NUM_PER_CIRCLE - 1 - ring_cnt, player_number);
+		simulation_renderer_render_frame(framebuffer);
+		ring_cnt++;
+	}
+	p->led_counter++;
+    return 0;
 }
 
 static uint32_t finish_animation(struct color *framebuffer, void *payload)
@@ -22,12 +67,13 @@ static uint32_t finish_animation(struct color *framebuffer, void *payload)
 struct animation *anim_game_start_init(struct color *framebuffer)
 {
     struct animation      *anim = malloc(sizeof(*anim));
+	static struct payload p;
 
     if (!anim)
         return NULL;
 
-    anim->payload          = NULL;
-    anim->speed            = 1000/60;
+    anim->payload          = &p;
+    anim->speed            = 60;
     anim->start_animation  = start_animation;
     anim->run_animation    = run_animation;
     anim->finish_animation = finish_animation;
