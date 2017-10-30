025979a35f808ac572f9b183c8b508a481fd4fcb
Simon Richter <simon.richter@hotmail.com>
2017-10-30 16:55:42 +0100

    changed to round start

--- include/anim/round_start.h
+++ include/anim/round_start.h
@@ -1,8 +1,9 @@
-#ifndef GAME_START_H
-#define GAME_START_H
+#ifndef ROUND_START_H
+#define ROUND_START_H
 
+#include "ll_game.h"
 #include "ll_led.h"
 
-struct animation *anim_game_start_init(struct color *framebuffer);
+struct animation *anim_round_start_init(struct color *framebuffer, struct game *game);
 
 #endif
