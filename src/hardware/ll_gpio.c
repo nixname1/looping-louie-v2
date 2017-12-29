29a7d5f74ccf14921838bf954e6793e6ffc8398c
Simon Richter <simon.richter@hotmail.com>
2017-12-29 16:53:36 +0100

    read new switch state every 100ms max.

--- src/hardware/ll_gpio.c
+++ src/hardware/ll_gpio.c
@@ -1,11 +1,18 @@
 #include <stm32f411xe.h>
 
+#include "hardware/ll_system.h"
 #include "hardware/ll_gpio.h"
 
+volatile uint64_t last_readout = 0;
+uint32_t last_state = 0;
 uint32_t ll_gpio_get_switch_state()
 {
-    uint32_t state = (GPIOB->IDR & GPIO_IDR_IDR_2) >> 2;
-    return state;
+	if(ll_system_get_systime() > last_readout + 100)
+	{
+		last_readout = ll_system_get_systime();
+		last_state = (GPIOB->IDR & GPIO_IDR_IDR_2) >> 2;
+	}
+    return last_state;
 }
 
 void ll_gpio_init()
